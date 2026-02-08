f9f21f3e4863d5ec709c4c6c9ac811a173931146
Katy Hinkelman <44682485+khinkelman@users.noreply.github.com>
2026-02-08 16:06:42 -0500

    Initial streamlit app

---
+++ app.py
@@ -0,0 +1,14 @@
+# SPDX-FileCopyrightText: 2026 Kathryn Hinkelamn (University of Vermont), 2026
+#
+# SPDX-License-Identifier: MIT
+
+# Get packages
+import streamlit as st
+
+# Configure page
+st.set_page_config(page_title="LCA Systems", layout="wide")
+# st.balloons()
+st.title("Life Cycle Assessment of Systems Modul")
+
+# Under construction
+st.warning(":building_construction: We are working on this page. Stay tuned.")
