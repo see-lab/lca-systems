75ec92d8e0f1f309f1c7bca0b3bfe1b9f9b3c056
khinkelman <kathryn.hinkelman@uvm.edu>
2026-03-24 12:35:31 -0400

    Text re-formatting

--- app.py
+++ app.py
@@ -24,20 +24,24 @@ st.header("Problem Set Up", divider=True)
 # Add columns
 col1, col2 = st.columns([2,1])
 
+# Text description
 with col1:
     st.markdown('''This module focuses on the life cycle assessment of residential 
                 storage systems, particularly for resilient heating solutions.
                 The goal is to compare two alternative storage technologies: (1) a battery-based (Li-ion) system and 
                 (2) a thermal storage system (silica sand, i.e., a sand battery).
-                The analysis will consider the environmental impacts associated with each system under various impact scenarios to understand: 
-                - Which system has the lowest environmental impact under different impact categories?
-                - What are the key drivers of impact for each system?
-                - How do the results inform design priorities for improving the sustainability of these systems?''')
+                ''')
     st.markdown("- **Goal:** Estimate biggest impacts to set design priorities.")
     st.markdown("- **Functional Unit:** 200 kWh of energy storage for residential heating over a 3-day outage and a 15-year lifetime.")
     st.markdown("- **System Boundary:** Scope 3 cradle-to-grave (materials & mfg, transport, & end of life).")
     st.markdown("- **Impact Units:** Several options (see drop down below).")
+    st.markdown("## Learning Objectives")
+    st.markdown("The analysis will consider the environmental impacts associated with each system under various impact scenarios to understand:")
+    st.markdown("- Which system has the lowest environmental impact under different impact categories?")
+    st.markdown("- What are the key drivers of impact for each system?")
+    st.markdown("- How do the results inform design priorities for improving the sustainability of these systems?")
 
+# System boundary image
 with col2:
     st.image("img/placeholder.png", caption="System boundaries (a) and (b).")
 
