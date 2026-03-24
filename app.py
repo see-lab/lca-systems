a83246d8bd9aa693f2942940832520a465cb4684
khinkelman <kathryn.hinkelman@uvm.edu>
2026-03-24 11:35:17 -0400

    3 column layout, testing data only

--- app.py
+++ app.py
@@ -4,13 +4,33 @@
 
 # Get packages
 import streamlit as st
+from numpy.random import default_rng as rng # For random numbers (DEVELOPMENT ONLY)
+
 
 # Configure page
 st.set_page_config(page_title="LCA Systems", layout="wide")
 # st.balloons()
 st.title("Life Cycle Assessment of Systems Module")
-st.subtitle("A Case Study on Residentential Storage Systems for Resilient Heating")
+st.header("A Case Study on Residentential Storage Systems for Resilient Heating", divider=True)
 
 
 # Under construction
-st.warning(":building_construction: We are working on this page. Stay tuned.")
+# st.warning(":building_construction: We are working on this page. Stay tuned.")
+
+# Add columns
+col1, col2, col3 = st.columns(3)
+
+# Column 1: Placeholder image (to be toggle buttons next)
+with col1:
+    st.header("A cat")
+    st.image("https://static.streamlit.io/examples/cat.jpg")
+
+# Random data for testing charts
+df = rng(0).standard_normal((10, 1))
+col2, col3 = st.columns([3, 1])
+
+col2.subheader("A wide column with a chart")
+col2.line_chart(df)
+
+col3.subheader("A narrow column with the data")
+col3.write(df)
