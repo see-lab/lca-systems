b511aec0dfadcd927f5ec65fe4597e6edbda68e6
khinkelman <kathryn.hinkelman@uvm.edu>
2026-03-24 12:06:57 -0400

    Header and footer info:

--- app.py
+++ app.py
@@ -12,10 +12,17 @@ st.set_page_config(page_title="LCA Systems", layout="wide")
 # st.balloons()
 st.title("Life Cycle Assessment of Systems Module")
 st.header("A Case Study on Residentential Storage Systems for Resilient Heating", divider=True)
+st.write("Authors: Kathryn Hinkelman, University of Vermont")
+st.write("Date: March 24, 2024")
 
+st.space(size="small")
 
-# Under construction
-# st.warning(":building_construction: We are working on this page. Stay tuned.")
+# Short description block
+st.subheader("Overview")
+st.write("This module focuses on the life cycle assessment of residential storage systems, particularly for resilient heating solutions.")
+
+# Main content
+st.space(size="small")
 
 # Add columns
 col1, col2, col3 = st.columns([1,2,1])
@@ -33,3 +40,11 @@ col2.line_chart(df)
 
 col3.subheader("A narrow column with the data")
 col3.write(df)
+
+#### NOT USED
+# Under construction
+# st.warning(":building_construction: We are working on this page. Stay tuned.")
+
+# Acknowledgements
+st.subheader("Acknowledgements")
+st.write("This educational module This research was supported by the U.S. National Science Foundation under Grant CBET-2501735. Any opinions, findings, and conclusions or recommendations expressed in this material are those of the authors and do not necessarily reflect the views of the National Science Foundation.")
