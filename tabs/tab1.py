1e261850b9f4adfd10167c522ed1a623f063a92e
khinkelman <kathryn.hinkelman@uvm.edu>
2026-03-27 00:25:50 -0400

    Added propane. Big graph improvement.

--- tabs/tab1.py
+++ tabs/tab1.py
@@ -4,6 +4,7 @@
 import streamlit as st
 import pandas as pd 
 import plotly.express as px
+from plotly.subplots import make_subplots as sp
 
 def show():
     # Main content
@@ -19,8 +20,8 @@ def show():
         impact_category = st.selectbox("Choose an impact method:", 
             ["Carbon Footprint", "ReCiPe Midpoint H", "ReCiPe Endpoint H"])
 
-        impact_location = st.selectbox("Choose a location:", 
-            ["Vermont", "Colorado", "California", "Wild Card"])
+        outage_frequency = st.selectbox("Choose an outage frequency:", 
+            ["Rare (5x/year)", "Moderate outages (10x/yr)", "Frequent outages (20x/yr)"])
 
         st.warning(":building_construction: Add Weighting Factors.")
 
@@ -41,31 +42,38 @@ def show():
 
     # Plotly chart (one type for midpoints; one for endpoints)
     if impact_category == "ReCiPe Midpoint H":
-        fig = px.bar(df, x='Midpoint', y=phases)
+        fig1 = px.bar(df, x='Midpoint', y=phases, color='System', 
+                      barmode='group', orientation='v',
+                      title="Comparison of Systems")
+        # fig = px.bar(df, y='Midpoint', x=phases, color='System', barmode='group', orientation='h')
+        fig2 = px.bar(df[df['System'] == 'Sand Battery'], 
+                      x='Midpoint', y=phases,
+                      title="Sand Battery Breakdown")
+        fig3 = px.bar(df[df['System'] == 'BESS'], 
+                      x='Midpoint', y=phases,
+                      title="BESS Breakdown")
+        # Update layout for both charts
+        fig1.update_layout(yaxis_title=units, legend_title=None)
+        fig1.update_layout(font=dict(family="Arial", size=26, color="black"))
+        fig2.update_layout(yaxis_title=units, legend_title=None)
+        fig2.update_layout(font=dict(family="Arial", size=26, color="black"))
+        fig3.update_layout(yaxis_title=units, legend_title=None)
+        fig3.update_layout(font=dict(family="Arial", size=26, color="black"))
+
+        # Display vertically
+        col2.plotly_chart(fig1)
+        col2.plotly_chart(fig2)
+        col2.plotly_chart(fig3)
     else:
-        fig = px.bar(df, x='System', y=phases)
+        fig = px.bar(df, x='System', y=phases,
+                     title="Comparison of Systems (Single Score)")
         fig.update_layout(yaxis_title=units, legend_title=None)
-        fig.update_layout(
-            font=dict(
-                family="Arial",
-                size=26, 
-                color="black"
-            )
-        )
-
-    col2.plotly_chart(fig)
-    # col2.bar_chart(df, x="System", y=phases, horizontal=False)
-
-    # Display data based on selected system(s). Get the system from the first column of the data frame and filter based on the selected systems.
-    # df = df[df["System"].isin(systems)]
-
-    # col2.subheader("A wide column with the charts")
-    # col2.line_chart(df)
-
-    # col3.subheader("A narrow column with the data")
-    # col3.write(df)
+        fig.update_layout(font=dict(family="Arial", size=26, color="black"))
+        
+        # Display chart
+        col2.plotly_chart(fig)
 
     st.divider()
     st.subheader("Selected Options")
     st.write("Impact Method:", impact_category)
-    st.write("Location:", impact_location)
+    st.write("Outage Frequency:", outage_frequency)
--- tabs/tab2.py
+++ tabs/tab2.py
@@ -13,18 +13,18 @@ def show():
         st.markdown("### Problem Set Up")
         st.markdown('''This module focuses on the life cycle assessment of residential 
                     storage systems, particularly for resilient heating solutions.
-                    The goal is to compare two alternative storage technologies: (1) a battery-based (Li-ion) system and 
-                    (2) a thermal storage system (silica sand, i.e., a sand battery).
+                    The goal is to compare two alternative storage technologies: (1) battery energy storage system (BESS) and 
+                    (2) a thermal storage system (silica sand, i.e., a "sand battery").
                     ''')
         st.markdown("- **Goal:** Estimate biggest impacts to set design priorities.")
         st.markdown("- **Functional Unit:** 200 kWh energy stored with a 15-year lifetime (3-day-long outages, 6 outages/year).")
         st.markdown("- **System Boundary:** Scope 3 cradle-to-grave (materials & mfg, transport, & end of life).")
         st.markdown("- **Impact Units:** Several options (see drop down below).")
         st.markdown("### Questions to Consider")
-        st.markdown("This tool analyzes the environmental impacts from each system under various impact scenarios and locations to understand:")
+        st.markdown("This tool analyzes the environmental impacts from each system under various impact scenarios to understand:")
         st.markdown("- How does environmental impact methods change the perception of each alternate's performance?")
         st.markdown("- What are the key impact drivers for each system?")
-        st.markdown("- How do the results vary across different locations (e.g., due to differences in electricity grid mix)?")
+        # st.markdown("- How do the results vary across different locations (e.g., due to differences in electricity grid mix)?")
         st.markdown("- How do the results inform design priorities for improving the sustainability of these systems?")
 
     # System boundary image
@@ -36,7 +36,7 @@ def show():
     # Acknowledgements
     st.subheader("Acknowledgements", divider=True)
     st.write('''The development of this educational module was supported by the U.S. 
-            National Science Foundation under Grant CBET-2501735. Any opinions, f
-            indings, and conclusions or recommendations expressed in this material 
+            National Science Foundation under Grant CBET-2501735. Any opinions, 
+            findings, and conclusions or recommendations expressed in this material 
             are those of the authors and do not necessarily reflect the views of the 
             National Science Foundation.''')
