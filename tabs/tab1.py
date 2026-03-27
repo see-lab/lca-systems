ac771e8512cee3e5466cbf253fd4d8bc81f97320
khinkelman <kathryn.hinkelman@uvm.edu>
2026-03-26 22:44:12 -0400

    Recipe for sand

--- tabs/tab1.py
+++ tabs/tab1.py
@@ -25,30 +25,34 @@ def show():
         st.warning(":building_construction: Add Weighting Factors.")
 
 
-        st.divider()
-        st.subheader("Selected Options")
-        st.write("Impact Method:", impact_category)
-        st.write("Location:", impact_location)
-
     # Save the path based on impact category selected.
-    PATH = f"./data/GWP100.csv" # MODIFY
-    df = pd.read_csv(PATH)
+    if impact_category == "Carbon Footprint":
+        PATH = f"./data/GWP100.csv"
+    elif impact_category == "ReCiPe Midpoint H":
+        PATH = f"./data/ReCiPe.csv"
+    else:
+        PATH = f"./data/ReCiPe.csv"
+    df = pd.read_csv(PATH)  # Save data frame from assigned path
 
     # Plot the charts
     phases = ["Materials & Mfg", "Transport", "Use", "End-of-Life"]
     units = "kg CO2e" if impact_category == "Carbon Footprint" else "Points"
     col2.subheader(f"{impact_category}")
 
-    # Plotly chart
-    fig = px.bar(df, x='System', y=phases)
-    fig.update_layout(yaxis_title=units, legend_title=None)
-    fig.update_layout(
-        font=dict(
-            family="Arial",
-            size=26, 
-            color="black"
+    # Plotly chart (one type for midpoints; one for endpoints)
+    if impact_category == "ReCiPe Midpoint H":
+        fig = px.bar(df, x='Midpoint', y=phases)
+    else:
+        fig = px.bar(df, x='System', y=phases)
+        fig.update_layout(yaxis_title=units, legend_title=None)
+        fig.update_layout(
+            font=dict(
+                family="Arial",
+                size=26, 
+                color="black"
+            )
         )
-    )
+
     col2.plotly_chart(fig)
     # col2.bar_chart(df, x="System", y=phases, horizontal=False)
 
@@ -60,3 +64,8 @@ def show():
 
     # col3.subheader("A narrow column with the data")
     # col3.write(df)
+
+    st.divider()
+    st.subheader("Selected Options")
+    st.write("Impact Method:", impact_category)
+    st.write("Location:", impact_location)
